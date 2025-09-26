e07dc6a5e4f24a79e28bceb44762518df9c248bc
Myth428 <118995403+Myth428@users.noreply.github.com>
2025-09-25 21:32:01 -0700

    B: added number_of_words function

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 
-
+# takes a filepath as input and return the contents of the file as a string
 def get_book_text(book):
     with open(book) as filepath:
         book = filepath.read()
         return book
-    
+
+# accepts text from books as a str
+def number_of_words(text):
+    return len(text.split())
+
+
 def main():
     contents = get_book_text("books/frankenstein.txt")
-    print(contents)
+    num_words = number_of_words(text=contents)
+    print(f"Found {num_words} total words")
 
     
 main()
- 
+
+
+     
